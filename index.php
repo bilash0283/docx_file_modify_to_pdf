697996a46b452ead07b78b7f33016bb67eff3386
bilash0283 <bilash0283@gmail.com>
2025-02-16 17:00:19 +0600

    autoloader setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is a heading of DOcx file modify</h1>
+    <input type="text" name="name" placeholder="Enter name"> <br>
+    <input type="text" name="email" placeholder="Enter email"><br>
+    <input type="text" name="phone" placeholder="Enter phone"><br>
+    <a href="docx_file_modify.php">Generate PDF</a>
+</body>
+</html>
