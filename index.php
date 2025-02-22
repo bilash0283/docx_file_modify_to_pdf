cf035ef368a5eda5d80c5bd6c319f8af5df4ff0d
bilash0283 <bilash0283@gmail.com>
2025-02-22 12:58:02 +0600

    index page input file setup

--- index.php
+++ index.php
@@ -1,20 +1,80 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>This is a heading of DOcx file modify</h1>
-    <form action="docx_file_modify.php" method="POST">
-        <input type="text" name="name" placeholder="Enter name" required> <br><br>
-        <input type="text" name="email" placeholder="Enter email" required><br><br>
-        <input type="text" name="phone" placeholder="Enter phone" required><br><br>
-        <input type="submit" name="btn" value="Submit" style="background-color:green;text-color:white; padding:10px 20px;">
-    </form>
+
+    <div class="container">
+        <div class="row my-5">
+            <div class="col-md-6 mx-auto">
+                <div class="card">
+                    <div class="card-head">
+                        <h2 class="text-center text-semibold text-success">Fill The From for Agent Agrement</h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="POST">
+                            <div class="my-2">
+                                <label for="name">Name</label>
+                                <input type="text" name="name" placeholder="Full Name" id="name"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="email">Email</label>
+                                <input type="email" name="email" placeholder="E-mail" id="email"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="country">Country Name</label>
+                                <input type="text" name="country" placeholder="Country Name" id="country"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="phone">Phone</label>
+                                <input type="number" name="phone" placeholder="Phone" id="phone"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="start_date">Agrement Date</label>
+                                <input type="date" name="start_date" id="start_date"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="company">Company Name</label>
+                                <input type="text" name="company" placeholder="Company Name" id="company"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="address">Company Address</label>
+                                <input type="text" name="address" placeholder="Company address" id="address"
+                                    class="form-control my-1">
+                            </div>
+                            <div class="my-2">
+                                <label for="position">Position</label>
+                                <input type="text" name="position" placeholder="Your Position" id="position"
+                                    class="form-control my-1">
+                            </div>
+                            <input type="button" name="btn" class="btn btn-success btn-sm" value="Submit" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
-</html> 
+</html>
