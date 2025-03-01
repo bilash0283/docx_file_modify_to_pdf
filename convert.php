a96eb8402386f13c5698ac8a9750a40ee1063a2b
bilash0283 <bilash0283@gmail.com>
2025-03-01 17:08:23 +0600

    convert pdf file but design formet change

--- convert.php
+++ convert.php
@@ -4,21 +4,33 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpWord\IOFactory;
 use Dompdf\Dompdf;
 
+// Load DOCX file
 $phpWord = IOFactory::load('output.docx');
 
+// Convert DOCX to HTML
 $htmlWriter = IOFactory::createWriter($phpWord, 'HTML');
 ob_start();
 $htmlWriter->save('php://output');
 $html = ob_get_clean();
 
+// Convert HTML to PDF
 $dompdf = new Dompdf();
 $dompdf->loadHtml($html);
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
-file_put_contents('output.pdf', $dompdf->output());
 
-echo "PDF Created Successfully!";
+// Output PDF to the browser with "Save As" option
+$pdfFileName = 'output.pdf';
+header('Content-Type: application/pdf');
+header('Content-Disposition: attachment; filename="' . $pdfFileName . '"');
+echo $dompdf->output();
 
+// JavaScript to auto-redirect after download
+echo "<script>
+    setTimeout(function() {
+        window.location.href = 'docx_file_modify.php'; // Change to your previous page
+    }, 3000); // Redirect after 3 seconds
+</script>";
 
-
+exit;
 ?>
